cea2f5fed7a525254c4de1cbc7279c47a98e82d4
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-08-07 20:47:26 -0300

    advancing

---
+++ aula8/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const mongodb = require('mongodb').MongoClient
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const url = `mongodb+srv://guilherme:${dbPassword}@cluster0.jiz8pb1.mongodb.net/?retryWrites=true&w=majority`
+
+mongodb.connect(url, (erro, banco) => {
+    if(erro) throw erro;
+    const dbo = banco.db("nodeJourney") // definimos nosso BD
+    const obj = {
+        curso: "Curso de node",
+        canal: "CFB Cursos"
+    }
+    const colecao = "cfbcursos"
+    dbo.collection(colecao).insertOne(obj, (erro, resultado) => {
+        if (erro){
+            throw  erro
+        }
+        console.log("1 novo curso inserido.")
+        banco.close()
+    })
+})
